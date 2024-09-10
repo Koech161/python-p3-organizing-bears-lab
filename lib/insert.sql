1029829553b6938afd9c5600d0f50e7ac5308ab2
Abel Koech <koechabel161@gmail.com>
2024-09-10 12:22:05 +0300

    query

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES  
+('Mr.Chocolate', 5, 'M', 'brown', 'freindly',1),
+('Rowdy', 3, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'white', 'calm', 1),
+('Sergeant Brown', 7, 'M', 'brown', 'grumpy', 1),
+('Melissa', 2, 'F', 'golden', 'playful', 1),
+('Grinch', 6, 'M', 'green', 'sly', 1),
+('Wendy', 4, 'F', 'pink', 'cheerful', 1),
+(NULL, NULL, 'F', NULL, NULL, 1);
